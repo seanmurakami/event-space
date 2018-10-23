e89dff440c69ba54779a90d314cdd1d5292d6cc6
Sean Murakami <murakami.sean@yahoo.com>
2018-10-23 11:28:33 -0700

    #2 updated the ShowCalendar component with a start and end date field that is updated when the user selects a date range on the calendar

--- src/calendar.js
+++ src/calendar.js
@@ -5,30 +5,52 @@ import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
 export default class ShowCalendar extends React.Component {
   constructor(props) {
     super(props)
+    const date = new Date()
     this.state = {
-      date: new Date()
+      date: date,
+      start: date.toDateString(),
+      end: date.toDateString()
     }
     this.onChange = this.onChange.bind(this)
   }
   onChange(date) {
-    this.setState({ date })
+    if (Array.isArray(date)) {
+      this.setState({ date, start: date[0].toDateString(), end: date[1].toDateString() })
+    }
+    else {
+      this.setState({ date })
+    }
   }
   render() {
     return (
       <Fragment>
         <h3 className="text-center">Set the Date For Your Event!</h3>
-        <Form>
-          <FormGroup>
-            <Label>Event Name:</Label>
-            <Input name="event-name" placeholder="Input event name" />
+        <Form autoComplete="off">
+          <FormGroup className="row w-75 mx-auto">
+            <div className="col">
+              <Label className="col-form-label">Start Date:</Label>
+              <Input
+                readOnly
+                name="event-name"
+                value={ this.state.start }/>
+            </div>
+            <div className="col">
+              <Label className="col-form-label">End Date:</Label>
+              <Input
+                readOnly
+                name="event-location"
+                value={ this.state.end }/>
+            </div>
           </FormGroup>
-          <FormGroup>
-            <Label>Event Location:</Label>
-            <Input name="event-location" placeholder="Input event location" />
-          </FormGroup>
-          <Calendar onChange={ this.onChange } value={ this.state.date } className="my-3 mx-auto"/>
-          <div className="d-flex justify-content-center">
-            <Button href="#create?step=date" className="w-50" color="primary">Continue</Button>
+          <Calendar
+            selectRange
+            calendarType="US"
+            onChange={ this.onChange }
+            value={ this.state.date }
+            className="my-3 mx-auto shadow"/>
+          <div className="d-flex justify-content-between">
+            <Button href="#" color="primary">Previous</Button>
+            <Button href="#create?step=description" color="primary">Continue</Button>
           </div>
         </Form>
       </Fragment>
