ad971a03f0cb8fea2e156f0c2bd445b991b6c4e3
Sean Murakami <murakami.sean@yahoo.com>
2018-10-26 10:24:23 -0700

    #4 updated the format of the calendar component for the calendar drop downs && created functions changeStart and changeEnd that allow the user to select both

--- src/wizard/calendar.js
+++ src/wizard/calendar.js
@@ -1,65 +1,58 @@
 import React, { Fragment } from 'react'
 import ButtonGroup from './button-group'
-import { Form, FormGroup, Row, Label, Input, Col } from 'reactstrap'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+import { Form, Row, Label, Col } from 'reactstrap'
+import 'react-datepicker/dist/react-datepicker.css'
 
 export default class ShowCalendar extends React.Component {
   constructor(props) {
     super(props)
-    const date = new Date()
     this.state = {
-      date,
-      startDate: date.toDateString(),
-      endDate: date.toDateString()
+      startDate: moment(),
+      endDate: moment()
     }
-    this.onChange = this.onChange.bind(this)
     this.eventDate = this.eventDate.bind(this)
-  }
-  onChange(date) {
-    if (Array.isArray(date)) {
-      this.setState({
-        date,
-        startDate: date[0].toDateString(),
-        endDate: date[1].toDateString()
-      })
-    }
-    else {
-      this.setState({ date })
-    }
+    this.changeStart = this.changeStart.bind(this)
+    this.changeEnd = this.changeEnd.bind(this)
   }
   eventDate(e) {
     e.preventDefault()
-    const formData = new FormData(e.target)
-    const dates = {
-      startDate: formData.get('event-start'),
-      endDate: formData.get('event-end')
-    }
-    this.props.eventDate(dates, 'lodging')
+    const userInfo = Object.assign({}, this.state)
+    const updateDates = Object.values(userInfo).map(moment => moment.toObject())
+    this.props.eventDate({startDate: updateDates[0], endDate: updateDates[1]}, 'lodging')
+  }
+  changeStart(date) {
+    this.setState({startDate: date})
+  }
+  changeEnd(date) {
+    this.setState({endDate: date})
   }
   render() {
     return (
       <Fragment>
         <h3 className="text-center">Set the Date For Your Event!</h3>
-        <Form autoComplete="off" onSubmit={ this.eventDate }>
-          <Row form>
-            <Col sm={6}>
-              <FormGroup>
-                <Label>Start Date:</Label>
-                <Input
-                  readOnly
-                  name="event-start"
-                  value={ this.state.startDate }/>
-              </FormGroup>
-            </Col>
-            <Col sm={6}>
-              <FormGroup>
+        <Form onSubmit={ this.eventDate }>
+          <div className="d-flex justify-content-center">
+            <Row>
+              <Col>
+                <Label className="mr-1">Start Date:</Label>
+                <DatePicker
+                  id="startDate"
+                  className="my-2 p-2 text-center"
+                  selected={this.state.startDate}
+                  onChange={this.changeStart} />
+              </Col>
+              <Col>
                 <Label>End Date:</Label>
-                <Input
-                  readOnly
-                  name="event-end"
-                  value={ this.state.endDate }/>
-              </FormGroup>
-            </Col>
-          </Row>
+                <DatePicker
+                  id="endDate"
+                  className="my-2 p-2 text-center"
+                  selected={this.state.endDate}
+                  onChange={this.changeEnd} />
+              </Col>
+            </Row>
+          </div>
           <ButtonGroup params="create?step=description"/>
         </Form>
       </Fragment>
