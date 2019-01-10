f329cf4ad1c4cb5954003b4b70cbc97eded7a5dd
Sean Murakami <murakami.sean@yahoo.com>
2019-01-09 20:08:52 -0800

    #58 updated server with a static call to the yelp api && made sure the response was logged && downloaded request-promise

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 require('dotenv/config')
 const server = require('json-server')
+const rp = require('request-promise')
+const bodyParser = require('body-parser')
 
 const app = server.create()
 const middleware = server.defaults()
 const router = server.router('db.json')
 
+const settings = {
+  url: 'https://api.yelp.com/v3/businesses/search?location=California&term=Starbucks',
+  headers: {Authorization: `Bearer ${process.env.KEY}`},
+  json: true
+}
+
+rp.get(settings)
+  .then(data => console.log(data))
+
+app.use(bodyParser.json())
 app.use(middleware)
 app.use(router)
 app.listen(process.env.PORT, () => {
